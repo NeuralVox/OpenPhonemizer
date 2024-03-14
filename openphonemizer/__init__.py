2bc717b07983cb0aa4246b79967b801a8d7f92ef
mrfakename <me@mrfake.name>
2024-03-14 09:58:38 -0700

    add gpu support

--- openphonemizer/__init__.py
+++ openphonemizer/__init__.py
@@ -36,12 +36,15 @@
 from dp.phonemizer import Phonemizer
 from cached_path import cached_path
 from num2words import num2words
-import re
+import re, torch
 class OpenPhonemizer:
     def __init__(self, model_checkpoint=None):
+        device = 'cpu'
+        if torch.cuda.is_available(): device = 'cuda'
+        if torch.backends.mps.is_available(): device = 'mps'
         if not model_checkpoint:
             model_checkpoint = str(cached_path('hf://openphonemizer/ckpt-001-alpha/prelim_ckpts/model_step_20k_fixed.pt'))
-        self.phonemizer = Phonemizer.from_checkpoint(model_checkpoint)
+        self.phonemizer = Phonemizer.from_checkpoint(model_checkpoint, device=device)
         self.pattern = re.compile(r'\d+')
     def _num_process(self, text):
         matches = self.pattern.findall(text)
